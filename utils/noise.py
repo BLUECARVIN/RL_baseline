93235d56f24c7a6678e99fee3abfec183e02d795
bluecarvin <bluecarvin@163.com>
2019-09-27 21:37:56 +0800

    Update noise.py

--- utils/noise.py
+++ utils/noise.py
@@ -3,7 +3,7 @@ import torch
 
 
 class RandomActionNoise:
-    def __init__(self, action_dim, mu=0, theta=0.1, sigma=0.2):
+    def __init__(self, action_dim, mu=0, theta=0.1, sigma=1):
         self.action_dim = action_dim
         self.mu = mu
         self.theta = theta
